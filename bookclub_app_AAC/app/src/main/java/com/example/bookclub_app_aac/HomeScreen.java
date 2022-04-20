518016e375b13b85588ffe1184c58709295f7acd
ashleyk82 <ashleykoluda@gmail.com>
2022-04-19 23:12:16 -0400

    toolbar

--- bookclub_app_AAC/app/src/main/java/com/example/bookclub_app_aac/HomeScreen.java
+++ bookclub_app_AAC/app/src/main/java/com/example/bookclub_app_aac/HomeScreen.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import androidx.appcompat.widget.SearchView;
+import androidx.appcompat.widget.Toolbar;
 
 public class HomeScreen extends AppCompatActivity
 {
@@ -16,7 +18,8 @@ public class HomeScreen extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
-
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
         messages = findViewById(R.id.messagesBtn);
         profile = findViewById(R.id.profileBtn);
         maps = findViewById(R.id.mapsBtn);
--- bookclub_app_AAC/app/src/main/java/com/example/bookclub_app_aac/LoginActivity.java
+++ bookclub_app_AAC/app/src/main/java/com/example/bookclub_app_aac/LoginActivity.java
@@ -95,7 +95,7 @@ public class LoginActivity extends AppCompatActivity {
         });
     }
     private void goMainActivity() {
-        Intent i = new Intent(this, MainActivity.class);
+        Intent i = new Intent(this, HomeScreen.class);
         startActivity(i);
     }
 }
