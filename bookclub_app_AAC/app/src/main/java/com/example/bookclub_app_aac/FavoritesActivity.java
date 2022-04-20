fce30db064b1ab6379fae1cd8c00b81a1dfb32fb
ashleyk82 <ashleykoluda@gmail.com>
2022-04-19 22:27:37 -0400

    add in page navigation

---
+++ bookclub_app_AAC/app/src/main/java/com/example/bookclub_app_aac/FavoritesActivity.java
@@ -0,0 +1,14 @@
+package com.example.bookclub_app_aac;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FavoritesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_favorites);
+    }
+}
--- bookclub_app_AAC/app/src/main/java/com/example/bookclub_app_aac/HomeScreen.java
+++ bookclub_app_AAC/app/src/main/java/com/example/bookclub_app_aac/HomeScreen.java
@@ -2,15 +2,56 @@ package com.example.bookclub_app_aac;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomeScreen extends AppCompatActivity
 {
+    Button messages, profile, maps, favorites;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+
+        messages = findViewById(R.id.messagesBtn);
+        profile = findViewById(R.id.profileBtn);
+        maps = findViewById(R.id.mapsBtn);
+        favorites = findViewById(R.id.favoritesBtn);
+
+        messages.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(HomeScreen.this, MessagesActivity.class);
+                startActivity(i);
+            }
+        });
+
+        profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(HomeScreen.this, ProfileActivity.class);
+                startActivity(i);
+            }
+        });
+
+        favorites.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(HomeScreen.this, FavoritesActivity.class);
+                startActivity(i);
+            }
+        });
+
+        maps.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(HomeScreen.this, MapsActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
